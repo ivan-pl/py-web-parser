b1daa7cdd929a5e2068bc3d46b3a41ec06aad7dd
ivan-pl <pleskaiv@gmail.com>
2022-11-03 10:06:24 +0300

    feat: add get_url

--- parse_links.py
+++ parse_links.py
@@ -1,16 +1,37 @@
-def args_is_correct():
-    pass
+import sys
+from getopt import getopt, GetoptError
 
 
-def get_options():
-    pass
-
-
-def get_links():
-    pass
+def get_url(argv):
+    url = None
+    try:
+        opts, args = getopt(argv, 'hu:', ["help", "url="])
+    except GetoptError:
+        show_error()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            show_help()
+            sys.exit()
+        if opt in ("-u, --url"):
+            url = arg
+    if (url is None):
+        show_error()
+        sys.exit(3)
+    return url
 
 
 def show_help():
+    print('Run "py parse_link.py -u https://ya.ru" '
+          'or py parse_link.py --url=https://ya.ru"')
+
+
+def show_error():
+    print('Invalid function arguments. Run "py parse_links.py -h" '
+          'to get help')
+
+
+def get_links():
     pass
 
 
@@ -18,21 +39,12 @@ def output_result():
     pass
 
 
-def main():
-    if (not args_is_correct()):
-        print('Invalid function arguments. Run "py parse_links.py -h"'
-              'to get help')
-        return
-
-    options = get_options()
-
-    if (options.help):
-        show_help()
-
-    links = get_links(options.url)
+def main(argv):
+    url = get_url(argv)
+    links = get_links(url)
     result = {key: get_links(key) for key in links}
-    output_result(result, options)
+    output_result(result)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
